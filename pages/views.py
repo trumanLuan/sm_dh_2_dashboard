58f1d0775bd9c391bc39b80405919404bb9960bd
Yizhao Luan <luanyz_leo@163.com>
2023-05-05 18:08:03 +0800

    新版本

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,14 @@ from .models import Employee
 # Create your views here.
 def index(request):
     return render(request, "index.html", {"employees": Employee.objects.all()})
+
+# views for search function in the homepage.
+
+
+# views for goto browse page from the sidebar.
+
+
+# views for goto search page from the sidebar.
+
+
+# views for goto help page from the sidebar.
